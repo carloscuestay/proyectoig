127f0d9aa0f9ea6ce10f188bfdb3cb7f22035ab7
CACY <carloscuestay@gmail.com>
2023-06-16 20:52:53 -0500

    commit 3 declaracion de archi db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')// driver mongo
 
-mongoose.connect('mongodb+srv://ig_cacy:TH9OajAEOMao3nGv@proyectoigdb.lh0hsiu.mongodb.net/')//conexxion a mongo
-.then(()=> console.log('Base de Datos Corriendo'))//como es asincrono se usa then para hacer seguimiento
-.catch((e) => console.log('Error en la Base de Datos'+e.message)) // capturar el error
+const user_db = process.env.USERDB
+const pass_db = process.env.PASSDB
+const name_db = process.env.NAMEDB
+const host_db = process.env.HOSTDB
+
+
+mongoose.connect(`mongodb+srv://${user_db}:${pass_db}@${name_db}.${host_db}`)//conexxion a mongo
+.then(()=> console.log(`Base de Datos Corriendo ${name_db}`))//como es asincrono se usa then para hacer seguimiento
+.catch(e => console.log(`Error en la Base de Datos ${e.message}`)) // capturar el error
